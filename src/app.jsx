6449c67416ae31e1266366660af656ce8a29c853
SEUNAGBEYE <agbeyeseun1@gmail.com>
2018-09-28 00:07:31 +0100

    chore(configuration): setup webpack and babel
    configure webpack for react
    configure webpack for bootstrap
    configure webpack for fontawesome
    configure babel
    
    [Finishes 160830962]

---
+++ src/app.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+ReactDOM.render(<p>Hello world</p>, document.getElementById('app'));
